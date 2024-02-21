f832b1369f8261fd7801dd0f2677b99a3e344cd6
Mohamed Mansour <hello@mohamedmansour.com>
2024-02-21 13:09:51 -0800

    feat: convert example button to fastelement

--- apps/example-shadowdom-app/src/button.ts
+++ apps/example-shadowdom-app/src/button.ts
@@ -1,11 +1,11 @@
-class AppButton extends HTMLElement {
+import { FASTElement, customElement } from '@internal/fast-element'
+
+const template = '<button><slot></slot></button>'
+
+@customElement({ name: 'app-button', template, cssModule: '/button.css' })
+export class AppButton extends FASTElement {
   static get observedAttributes() {
     return ['appearance']
-  }
-
-  constructor() {
-    super()
-    this.attachShadow({ mode: 'open' })
   }
 
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
@@ -22,45 +22,7 @@ class AppButton extends HTMLElement {
   }
 
   connectedCallback() {
-    this.shadowRoot!.innerHTML = `
-      <style>
-        :host {
-          --background-color: #007bff;
-          --text-color: #ffffff;
-          --border-color: transparent;
-        }
-
-        @media (prefers-color-scheme: dark) {
-          :host {
-            --background-color: #0066cc;
-            --text-color: #ffffff;
-            --border-color: transparent;
-          }
-        }
-
-        button {
-          background-color: var(--background-color);
-          color: var(--text-color);
-          border: 2px solid var(--border-color);
-          padding: 10px 20px;
-          text-align: center;
-          text-decoration: none;
-          display: inline-block;
-          font-size: 16px;
-          margin: 4px 2px;
-          cursor: pointer;
-        }
-
-        button.secondary {
-          background-color: transparent;
-          outline: 1px solid var(--text-color);
-        }
-      </style>
-      <button><slot></slot></button>
-    `
-
+    super.connectedCallback()
     this.updateStyle(this.getAttribute('appearance') || '')
   }
 }
-
-customElements.define('app-button', AppButton)
