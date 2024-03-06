d77a6a88bdb254a4a5d6c677cc9b227aab3525d1
Mohamed Mansour <hello@mohamedmansour.com>
2024-03-06 01:17:46 -0800

    fix(rust): add string and array support

--- packages/fast-btr-parser-rust/src/safe_evaluate_expression.rs
+++ packages/fast-btr-parser-rust/src/safe_evaluate_expression.rs
@@ -1,10 +1,10 @@
 use crate::find_value_by_dotted_path::find_value_by_dotted_path;
 use lazy_static::lazy_static;
 use regex::Regex;
-use serde_json::Value;
 use serde_json::Number;
+use serde_json::Value;
 
-const OPERATORS: [&str; 10] =  ["&&", "||", "==", ">=", "<=", ">", "<", "!", "(", ")"];
+const OPERATORS: [&str; 10] = ["&&", "||", "==", ">=", "<=", ">", "<", "!", "(", ")"];
 
 lazy_static! {
     static ref RE: Regex = {
@@ -86,15 +86,13 @@ fn evaluate(tokens: &mut Vec<&str>, state: &Value) -> Value {
 }
 
 fn value_to_bool(value: Option<Value>) -> bool {
-    value.map_or(false, |v| {
-        match v {
-            Value::Bool(b) => b,
-            Value::Number(n) => n.as_f64() != Some(0.0),
-            Value::String(s) => !s.is_empty(),
-            Value::Array(a) => !a.is_empty(),
-            Value::Object(o) => !o.is_empty(),
-            _ => false,
-        }
+    value.map_or(false, |v| match v {
+        Value::Bool(b) => b,
+        Value::Number(n) => n.as_f64() != Some(0.0),
+        Value::String(s) => !s.is_empty(),
+        Value::Array(a) => !a.is_empty(),
+        Value::Object(o) => !o.is_empty(),
+        _ => false,
     })
 }
 
@@ -109,7 +107,7 @@ fn apply_operator(value1: Option<Value>, operator: Option<&str>, value2: Value) 
             (Some(Value::Array(a1)), Value::Array(a2)) => Value::Bool(a1 == a2),
             (Some(Value::Object(o1)), Value::Object(o2)) => Value::Bool(o1 == o2),
             _ => Value::Bool(false),
-        }
+        },
         Some("!=") => Value::Bool(value1 != Some(value2)),
         Some(">") => {
             if let (Some(Value::Number(n1)), Value::Number(n2)) = (value1, value2) {
@@ -153,16 +151,21 @@ fn apply_operator(value1: Option<Value>, operator: Option<&str>, value2: Value) 
             let b = value_to_bool(Some(value2));
             Value::Bool(!b)
         }
-        _ => value2
+        _ => value2,
     }
 }
 
-// Parses a token into a Value. 
+// Parses a token into a Value.
 fn parse_value(token: &str, state: &Value) -> Value {
     if let Ok(number) = token.parse::<f64>() {
         let json_number = Number::from_f64(number).unwrap();
         return Value::Number(json_number);
     }
+
+    if token.starts_with("\"") || token.starts_with("'") {
+        return Value::String(token[1..token.len() - 1].to_string());
+    }
+
     match token {
         "true" => Value::Bool(true),
         "false" => Value::Bool(false),
@@ -176,7 +179,6 @@ fn parse_value(token: &str, state: &Value) -> Value {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -189,17 +191,43 @@ mod tests {
     #[test]
     fn test_parse_expression() {
         let test_cases = vec![
-            TestCaseParse { expression: "a && b || c", expected: vec!["a", "&&", "b", "||", "c"] },
-            TestCaseParse { expression: "d == e", expected: vec!["d", "==", "e"] },
-            TestCaseParse { expression: "f > g", expected: vec!["f", ">", "g"] },
-            TestCaseParse { expression: "k&&l", expected: vec!["k", "&&", "l"] },
-            TestCaseParse { expression: "m  ||  n", expected: vec!["m", "||", "n"] },
-            TestCaseParse { expression: "o > p && q <= r || s == t", expected: vec!["o", ">", "p", "&&", "q", "<=", "r", "||", "s", "==", "t"] },
-            TestCaseParse { expression: "foo.bar && bar", expected: vec!["foo.bar", "&&", "bar"] }
+            TestCaseParse {
+                expression: "a && b || c",
+                expected: vec!["a", "&&", "b", "||", "c"],
+            },
+            TestCaseParse {
+                expression: "d == e",
+                expected: vec!["d", "==", "e"],
+            },
+            TestCaseParse {
+                expression: "f > g",
+                expected: vec!["f", ">", "g"],
+            },
+            TestCaseParse {
+                expression: "k&&l",
+                expected: vec!["k", "&&", "l"],
+            },
+            TestCaseParse {
+                expression: "m  ||  n",
+                expected: vec!["m", "||", "n"],
+            },
+            TestCaseParse {
+                expression: "o > p && q <= r || s == t",
+                expected: vec!["o", ">", "p", "&&", "q", "<=", "r", "||", "s", "==", "t"],
+            },
+            TestCaseParse {
+                expression: "foo.bar && bar",
+                expected: vec!["foo.bar", "&&", "bar"],
+            },
         ];
 
         for test_case in test_cases {
-            assert_eq!(parse_expression(test_case.expression), test_case.expected, "Failed on expression: {}", test_case.expression);
+            assert_eq!(
+                parse_expression(test_case.expression),
+                test_case.expected,
+                "Failed on expression: {}",
+                test_case.expression
+            );
         }
     }
 
@@ -227,25 +255,87 @@ mod tests {
         });
 
         let test_cases = vec![
-            TestCaseEval { expression: "name.first && age", expected: true },
-            TestCaseEval { expression: "age == 30", expected: true },
-            TestCaseEval { expression: "favorite.categories.music && favorite.categories.movies", expected: true },
-            TestCaseEval { expression: "age > 10", expected: true },
-            TestCaseEval { expression: "name && name.first", expected: true },
-            TestCaseEval { expression: "is_student", expected: true },
-            TestCaseEval { expression: "is_student && name.first", expected: true },
-            TestCaseEval { expression: "!is_student || true", expected: true },
-            TestCaseEval { expression: "name.first &&", expected: false },
-            TestCaseEval { expression: "name.middle", expected: false },
-            TestCaseEval { expression: "name.first && (age == 31)", expected: false },
-            TestCaseEval { expression: "name.first && false", expected: false },
-            TestCaseEval { expression: "\"a\" == \"b\"", expected: false },
-            TestCaseEval { expression: "!is_student", expected: false },
-            TestCaseEval { expression: "!is_student && name.first", expected: false },
+            TestCaseEval {
+                expression: "name.first && age",
+                expected: true,
+            },
+            TestCaseEval {
+                expression: "age == 30",
+                expected: true,
+            },
+            TestCaseEval {
+                expression: "favorite.categories.music && favorite.categories.movies",
+                expected: true,
+            },
+            TestCaseEval {
+                expression: "favorite.categories.music == favorite.categories.music",
+                expected: true,
+            },
+            TestCaseEval {
+                expression: "age > 10",
+                expected: true,
+            },
+            TestCaseEval {
+                expression: "name && name.first",
+                expected: true,
+            },
+            TestCaseEval {
+                expression: "is_student",
+                expected: true,
+            },
+            TestCaseEval {
+                expression: "is_student && name.first",
+                expected: true,
+            },
+            TestCaseEval {
+                expression: "!is_student || true",
+                expected: true,
+            },
+            TestCaseEval {
+                expression: "\"a\" == \"a\"",
+                expected: true,
+            },
+            TestCaseEval {
+                expression: "name.first &&",
+                expected: false,
+            },
+            TestCaseEval {
+                expression: "name.middle",
+                expected: false,
+            },
+            TestCaseEval {
+                expression: "name.first && (age == 31)",
+                expected: false,
+            },
+            TestCaseEval {
+                expression: "name.first && false",
+                expected: false,
+            },
+            TestCaseEval {
+                expression: "favorite.categories.music == favorite.categories.movies",
+                expected: false,
+            },
+            TestCaseEval {
+                expression: "\"a\" == \"b\"",
+                expected: false,
+            },
+            TestCaseEval {
+                expression: "!is_student",
+                expected: false,
+            },
+            TestCaseEval {
+                expression: "!is_student && name.first",
+                expected: false,
+            },
         ];
-        
+
         for test_case in test_cases {
-            assert_eq!(safe_evaluate_expression(test_case.expression, &data), test_case.expected, "Failed on expression: {}", test_case.expression);
+            assert_eq!(
+                safe_evaluate_expression(test_case.expression, &data),
+                test_case.expected,
+                "Failed on expression: {}",
+                test_case.expression
+            );
         }
     }
 }
