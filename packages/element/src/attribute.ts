4d65368c0044094beb1664e866aef9604e711cd6
Mohamed Mansour <hello@mohamedmansour.com>
2024-03-13 13:17:32 -0700

    chore: add peer dependencies for esbuild/typescript

--- packages/element/src/attribute.ts
+++ packages/element/src/attribute.ts
@@ -1,11 +1,9 @@
 export function attr(target: HTMLElement, propertyKey: string) {
   Object.defineProperty(target, propertyKey, {
     get: function() {
-      console.log('get', this, this.value, this.getAttribute(propertyKey))
       return this.getAttribute(propertyKey)
     },
     set: function(newValue: string) {
-      console.log('set', this, this.value, this.getAttribute(propertyKey))
       const oldValue = this.getAttribute(propertyKey)
       this.setAttribute(propertyKey, newValue)
       if (this.hydrated && this[`${propertyKey}Changed`]) {
