49b48bd772b25b9bace3373246307ef8a15f52c0
Mohamed Mansour <hello@mohamedmansour.com>
2024-03-12 17:59:23 -0700

    chore: add scripts to publish packages

--- scripts/Publish-Npm-Packages.ps1
+++ scripts/Publish-Npm-Packages.ps1
@@ -1,13 +1,21 @@
-$rootPath = "./packages"
+$rootPath = Resolve-Path "./packages"
 $directories = Get-ChildItem -Path $rootPath -Directory
 
 Push-Location
 
 foreach ($dir in $directories) {
-    Set-Location -Path $dir.FullName
-    Write-Host "Publishing package: $($dir.Name)"
-    pnpm publish --access public
-    Set-Location -Path $rootPath
+    try {
+        Set-Location -Path $dir.FullName -ErrorAction Stop
+        Write-Host "Publishing package: $($dir.Name)"
+        pnpm publish --access public
+        Set-Location -Path $rootPath -ErrorAction Stop
+    } catch {
+        Write-Error "An error occurred while publishing the package: $($dir.Name)"
+        Write-Error $_.Exception.Message
+    } finally {
+        # Return to the root path of your packages directory
+        Set-Location -Path $rootPath
+    }
 }
 
 Pop-Location
